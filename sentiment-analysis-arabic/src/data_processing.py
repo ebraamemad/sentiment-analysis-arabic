c57824d48e42811cff575f1ae72a0019ad621d33
ebraamemad <beboe668@gmail.com>
2025-05-01 18:47:25 +0300

    EDA

---
+++ sentiment-analysis-arabic/src/data_processing.py
@@ -0,0 +1,11 @@
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import pandas as pd
+df= pd.read_csv('E:\projects\sentiment-analysis-arabic\sentiment-analysis-arabic\data\processed\ready_cleaned_arabic_data.csv')
+
+# Apply label encoding over the labels
+lable_encoder = preprocessing.LabelEncoder()
+encoded_labels =lable_encoder.fit_transform(df["class"])
+df['class']=encoded_labels
+
+X_train, X_validation, y_train, y_validation=train_test_split(df['tweet'], df['class'], test_size=0.2, random_state=42)
